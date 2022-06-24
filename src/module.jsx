7d5eaa547331620500c2a3f5c07a85c9d136b6ca
Нурсултан <nursultanerzhan9301@gmail.com>
2022-06-25 01:17:50 +0600

    1

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import nurs from './img/logo.svg';
+
+const Module = () => (
+    <>
+        <h1>Hello React!</h1>
+        <img width="400" src={nurs} />
+    </>
+);
+
+export default Module;
